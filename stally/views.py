1a37d9077fb1fbf79bef8ba5a8389226fa1fb3d1
eyefox <lucas.nicolaus@uni-oldenburg.de>
2016-11-05 15:57:02 +0100

    removed double sorting

--- stally/views.py
+++ stally/views.py
@@ -7,17 +7,17 @@ def campaign_list(request):
 
 def campaign_character_list(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
-    characters = campaign.characters_without_pokemon().exclude(pk__in=campaign.npcs()).order_by('name')
+    characters = campaign.characters_without_pokemon().exclude(pk__in=campaign.npcs())
     return render(request, 'stally/campaign_character_list.html', {'campaign': campaign, 'characters': characters})
 
 def character_pokemon_list(request, pk):
     character = get_object_or_404(Character, pk=pk)
-    pokemons = character.pokemons.order_by('name')
+    pokemons = character.pokemons
     return render(request, 'stally/character_pokemon_list.html', {'character': character, 'pokemons': pokemons})
 
 def campaign_pokemon_list(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
-    pokemons = campaign.pokemons().order_by('name')
+    pokemons = campaign.pokemons()
     return render(request, 'stally/campaign_pokemon_list.html', {'campaign': campaign, 'pokemons': pokemons})
 
 def campaign_npc_list(request, pk):

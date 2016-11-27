9d0d5432ea8853fc135d4355bb7678b93b288884
eyefox <eyefox42@gmail.com>
2016-11-27 18:23:11 +0100

    moved templates to separate folders

--- stally/views.py
+++ stally/views.py
@@ -6,7 +6,7 @@ from .forms import PokemonForm
 
 def campaign_list(request):
     campaigns = Campaign.objects.all().order_by('start_date').reverse()
-    return render(request, 'stally/campaign_list.html',
+    return render(request, 'stally/list_pages/campaign_list.html',
                   {'campaigns': campaigns})
 
 
@@ -14,45 +14,46 @@ def campaign_character_list(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
     characters = campaign.characters_without_pokemon().exclude(
         pk__in=campaign.npcs())
-    return render(request, 'stally/campaign_character_list.html',
+    return render(request, 'stally/list_pages/campaign_character_list.html',
                   {'campaign': campaign, 'characters': characters})
 
 
 def character_pokemon_list(request, pk):
     character = get_object_or_404(Character, pk=pk)
     pokemons = character.pokemons
-    return render(request, 'stally/character_pokemon_list.html',
+    return render(request, 'stally/list_pages/character_pokemon_list.html',
                   {'character': character, 'pokemons': pokemons})
 
 
 def campaign_pokemon_list(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
     pokemons = campaign.pc_pokemon()
-    return render(request, 'stally/campaign_pokemon_list.html',
+    return render(request, 'stally/list_pages/campaign_pokemon_list.html',
                   {'campaign': campaign, 'pokemons': pokemons})
 
 
 def campaign_npc_list(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
-    return render(request, 'stally/campaign_npc_list.html',
+    return render(request, 'stally/list_pages/campaign_npc_list.html',
                   {'campaign': campaign})
 
 
 def campaign_detail(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
-    return render(request, 'stally/campaign_detail.html',
+    return render(request, 'stally/detail_pages/campaign_detail.html',
                   {'campaign': campaign})
 
 
 def character_detail(request, pk):
     character = get_object_or_404(Character, pk=pk)
-    return render(request, 'stally/character_detail.html',
+    return render(request, 'stally/detail_pages/character_detail.html',
                   {'character': character})
 
 
 def pokemon_detail(request, pk):
     pokemon = get_object_or_404(Pokemon, pk=pk)
-    return render(request, 'stally/pokemon_detail.html', {'pokemon': pokemon})
+    return render(request, 'stally/detail_pages/pokemon_detail.html',
+                  {'pokemon': pokemon})
 
 
 @login_required
@@ -66,7 +67,8 @@ def pokemon_new(request):
             return redirect('pokemon_detail', pk=pokemon.pk)
     else:
         form = PokemonForm()
-    return render(request, 'stally/pokemon_edit.html', {'form': form})
+    return render(request, 'stally/edit_pages/pokemon_edit.html',
+                  {'form': form})
 
 
 @login_required
@@ -81,12 +83,14 @@ def pokemon_edit(request, pk):
             return redirect('pokemon_detail', pk=pokemon.pk)
     else:
         form = PokemonForm(instance=pokemon)
-    return render(request, 'stally/pokemon_edit.html', {'form': form})
+    return render(request, 'stally/edit_pages/pokemon_edit.html',
+                  {'form': form})
 
 
 def player_detail(request, pk):
     player = get_object_or_404(Player, pk=pk)
-    return render(request, 'stally/player_detail.html', {'player': player})
+    return render(request, 'stally/detail_pages/player_detail.html',
+                  {'player': player})
 
 
 def about(request):

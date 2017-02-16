17b047d468770bce35d729146cd72702d9d9ccbe
eyefox <eyefox42@gmail.com>
2017-02-16 19:31:59 +0100

    changed project name to mwm-db

--- mwm-db/wsgi.py
+++ mwm-db/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dally_stats project.
+WSGI config for mwm-db project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dally_stats.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mwm-db.settings")
 
 application = get_wsgi_application()
